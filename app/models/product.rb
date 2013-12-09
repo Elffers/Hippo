79317829426a458e77f37da779d2d1f6a495cc69
Anne Kimsey <anne.kimsey@gmail.com>
2013-12-09 15:07:13 -0800

    Added relationships in products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  belongs_to :merchant
+  validation :name #unique...method
+  has_many :categories, through: :category_products
+  has_many :category_products
 end
