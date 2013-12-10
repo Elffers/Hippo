621b27593b7a0f30c8c8017d9669aa11c7a91873
Whitney Levis <ms.whitney.levis@gmail.com>
2013-12-10 13:37:31 -0800

    push for debugging

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
-   validates :name, presence: true, uniqueness: { case_sensitive: false }
-   validates :price, presence: true, numericality: { greater_than: 0 }
-   validates :user_id, presence: true
-   belongs_to :user
-   has_many :categories, through: :category_products
-   has_many :category_products
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :user_id, presence: true
+  belongs_to :user
+  has_many :categories, through: :category_products
+  has_many :category_products
 end
