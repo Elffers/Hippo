7a87b056f6bbb66b0cccc7c7acbcf9d3eff76241
Anne Kimsey <anne.kimsey@gmail.com>
2013-12-09 16:07:24 -0800

    set up rspec and guard

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209221059) do
+ActiveRecord::Schema.define(version: 20131209232004) do
 
   create_table "products", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
     t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
