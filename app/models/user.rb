586fcc59db8fd0e15bd5cdf3b022a65d4a0cf6fb
Anne Kimsey <anne.kimsey@gmail.com>
2013-12-11 14:46:40 -0800

    added user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
   validates_presence_of :email, :on => :create
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
