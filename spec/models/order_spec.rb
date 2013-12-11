990558a35e627d0b320bdef5dd1e192668d67d0c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-11 15:40:33 -0800

    OrderProduct table, rpsec for belongs_to and has_many through relationships

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe Order do
   before do 
-    @product = Product.create(name: "shoes", price: 1.00, user_id: 1)
+    @product = Product.create(name: "shoes", price: 1.00, user_id: 1, description: "put it on yo feet", inventory: 3, retired: true)
     @user = User.create(email: "blake.jennings@gmail.com", password: "dog", password_confirmation: "dog")
     @order = Order.create(product_id: @product.id, user_id: @user.id)
   end
 
-  it "belongs to user" do
-    expect(@order.user).to eq @user
+  it "has a quantity greater than 0" do
+    order = Order.new(quantity: 0)
+    expect(order).to_not be_valid
   end
 end
 
