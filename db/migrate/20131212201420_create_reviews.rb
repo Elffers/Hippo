09412a0d33ab1230d66553b1f857de9bde8fe5b3
Anne Kimsey <anne.kimsey@gmail.com>
2013-12-12 12:44:30 -0800

    Added reviews model and wrote some specs

---
+++ db/migrate/20131212201420_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer, :rating
+      t.integer, :product_id
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
