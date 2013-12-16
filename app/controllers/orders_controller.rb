64c89cf10291f95ea50d8a6228ebd61cfe302d63
Your Name <blake.jennings@gmail.com>
2013-12-16 10:53:39 -0800

    before action order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-  
+  before_action :set_order, except: [:new, :create]
+
   def new
     @order = Order.new
   end
@@ -17,23 +18,15 @@ class OrdersController < ApplicationController
   end
 
   def show
-    # @order = Order.find(params[:id])
-    @order = current_order
   end
   
   def add_product
-    # @product = Product.find(params[:product_id])
     @orderproduct = OrderProduct.new(order_id: current_order.id, product_id: params[:product_id])
-    # @orderproduct.save
-    @products = showproducts #we might want to change this. Product.last.name
+    @products = current_order.products #this returns objects-- iterate in HTML
       if @orderproduct.save
-      # if current_order  
-      # else
-      #   true
-      # end
       redirect_to order_path(current_order) #changes url
     else 
-      flash.now[:notice] = "There was a problem adding this item to the cart." #render doesn't show notice b/c next page thing
+      flash.now[:notice] = "There was a problem adding this item to the cart." #render doesn't show notice b/c generates page first
       render :"/product/show"      
     end
   end
@@ -41,15 +34,9 @@ class OrdersController < ApplicationController
 
 private
 
-  def showproducts #we actually want this in the HTML
-    current_order.products.map do |product|
-      [product.name]
-    end
+  def set_order
+    @order = current_order
   end
-    # def orderproduct_params
-  #   params.require(:order_products).permit(:order_id, :product_id, :quantity)
-  # end
-
 end
 
 
