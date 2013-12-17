c7115af97596f17d1cd8698b55b49622f50230c9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-17 10:06:30 -0800

    added comments for creating category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,16 @@ class CategoriesController < ApplicationController
   def new
     require_login
     @category = Category.new
+    #@product = Product.find(params[:product_id]) 
   end
 
   def create
+    #@product = Product.find(params[:product_id])
     @category = Category.new(category_params)
     @category.save
+    #redirect_to edit_product_path(@product.id) 
+    #needs some way to remember which product 
+    #you were editing at the time you wanted to create this new category
   end
 
   def index
