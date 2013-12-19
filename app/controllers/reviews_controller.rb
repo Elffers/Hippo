548b029665452f2db0aa3a5009f82a93fae1b3f5
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-18 16:03:03 -0800

    trying to fix reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,21 +7,28 @@ class ReviewsController < ApplicationController
  
   def create # Are we going to have a problem if current_user.id == nil here?
     @review = Review.new(review_params)
-    if session[:user_id] == @product.user_id
-      flash[:notice] = "You can't review your own products. That's cheating. >:("
-    else
-      @review = Review.new(review_params)
-      @review.save
-      if @review.save && current_user
-        redirect_to "/reviews/#{@review.id}", notice: "We hear ya! Review posted as #{user.find(session[:user_id])}."
-      elsif @review.save
-        redirect_to "/reviews/#{@review.id}", notice: "We hear ya! Posted as guest."
-      end    
+    @review.product_id = params[:product_id]
+    @product = Product.find_by(params[:product_id])
+    # if session[:user_id] == @product.user_id
+    #   flash[:notice] = "You can't review your own products. That's cheating. >:("
+    #   redirect_to "/products/#{@product.id}"
+    # else
+    #   @review = Review.new(review_params)
+    #   @review.save
+    #   if @review.save && current_user
+    #     redirect_to "/reviews/#{@review.id}", notice: "We hear ya! Review posted as #{user.find(session[:user_id])}."
+    #   elsif @review.save
+    #     redirect_to "/reviews/#{@review.id}", notice: "We hear ya! Posted as guest."
+    #   end    
+    
+    if @review.save
+      flash[:notice] = "You have successfully posted this review"
+      redirect_to "/products/#{@product.id}" 
     end
   end
 
   def show
-    @review = Review.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def edit   
