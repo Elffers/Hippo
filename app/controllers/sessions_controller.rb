d9c14d0215c3b067826a140aa7b1e5edfba5b86c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-19 14:17:51 -0800

    cleaned up things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id 
       # this should attach order to user if signed in. 
       current_order.update(user_id:current_user.id) if current_user 
-      #needs to work for buying things then signing in, or signing in then buying things
       redirect_to root_path, notice: "Hip Hipporay! You are now logged in as #{user.name}!"
     else
       render :new, notice: "Invalid email or password :("
