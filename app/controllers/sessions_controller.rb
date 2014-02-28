c1eede52943e45befa382c7f873fa6f5b82dead2
Melinda Mizuta <mkmizuta@gmail.com>
2014-02-27 16:03:57 -0800

    rubocop down 15 to 4 for app/controllers/sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,25 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id 
-      # this should attach order to user if signed in. 
-      current_order.update(user_id:current_user.id) if current_user 
-      redirect_to root_path, notice: "Hip Hipporay! You are now logged in as #{user.name}!"
-      # To set previous orderproducts into current order if not checked out- should maybe be a private method?
+      session[:user_id] = user.user_id
+      # this should attach order to user if signed in.
+      current_order.update(user_id: current_user.id) if current_user
+      redirect_to root_path, notice: "Hip Hipporay! You are now logged in as
+                                     #{user.name}!"
+      # To set previous orderproducts into current order if not checked out-
+      # should maybe be a private method?
       if user.orders != []
         user.orders.each do |order|
           if order.status == "pending" && order != current_order
             order.order_products.each do |op|
-              op.update(order_id:current_order.id)
+              op.update(order_id: current_order.id)
               order.destroy
             end
           end
         end
       end
-      # also should have private method here to destroy any OPs put into the current order (i.e. as a guest) 
-      # if the user owns the product
+      # also should have private method here to destroy any OPs put into
+      # the current order (i.e. as a guest) if the user owns the product
     else
       render :new, notice: "Invalid email or password :("
     end
@@ -36,7 +38,7 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     session[:order_id] = nil
 
-    redirect_to root_path, notice: "You are now logged out! Hip Hop awaaaaaayyy!"
+    redirect_to root_path, notice: "You are now logged out! Hip Hop
+                                    awaaaaaayyy!"
   end
-
 end
